903c07714399879ebb9a0eac71dc265ddbde9c1a
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2021-08-10 15:16:31 +0200

    Improve example

--- serde_wasm_bindgen/lib.rs
+++ serde_wasm_bindgen/lib.rs
@@ -1,16 +1,24 @@
+use serde::ser::Serialize;
 use serde_json::Value;
 use wasm_bindgen::prelude::*;
 
-type Result<T> = std::result::Result<T, serde_wasm_bindgen::Error>;
+// type Result<T> = std::result::Result<T, serde_wasm_bindgen::Error>;
 
-pub fn to_value<T: serde::ser::Serialize>(value: &T) -> Result<JsValue> {
-    value.serialize(&serde_wasm_bindgen::Serializer::new().serialize_maps_as_objects(true))
-}
+// pub fn to_value<T: serde::ser::Serialize>(value: &T) -> Result<JsValue> {
+//     value.serialize(&serde_wasm_bindgen::Serializer::new().serialize_maps_as_objects(true))
+// }
+
+// #[wasm_bindgen]
+// pub fn parse(s: &str) -> JsValue {
+//     let value: Value = serde_json::from_str(s).unwrap();
+//     to_value(&value).unwrap()
+// }
 
 #[wasm_bindgen]
 pub fn parse(s: &str) -> JsValue {
     let value: Value = serde_json::from_str(s).unwrap();
-    to_value(&value).unwrap()
+    let serializer = serde_wasm_bindgen::Serializer::new().serialize_maps_as_objects(true);
+    value.serialize(&serializer).unwrap()
 }
 
 #[wasm_bindgen]
